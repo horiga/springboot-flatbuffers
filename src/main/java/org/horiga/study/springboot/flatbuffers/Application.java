96b3d68c70448bc4029c8e2be4e14bf3d5d82b78
Hiroyuki Horigami <hiroyuki.horigami@linecorp.com>
2015-09-11 21:42:59 +0900

    Change Websocket to HTTP

--- src/main/java/org/horiga/study/springboot/flatbuffers/Application.java
+++ src/main/java/org/horiga/study/springboot/flatbuffers/Application.java
@@ -1,12 +1,19 @@
 package org.horiga.study.springboot.flatbuffers;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+@Slf4j
+public class Application implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
     }
 }
